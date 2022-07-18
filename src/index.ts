4ae8c957144ada2524ccef543304433beb6b6d71
NikDoe <nikd0e@yandex.ru>
2022-07-18 21:25:57 +0300

    feature(sort): sorting implementation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,26 @@
 class Sorter {
 	constructor(public collection: number[]) {}
 
-	sort(): void {}
+	sort(): void {
+		const { length } = this.collection;
+		let isSwapped = false;
+
+		for (let i = 0; i < length; i++) {
+			isSwapped = false;
+
+			for (let j = 0; j < length; j++) {
+				if (this.collection[j] > this.collection[j + 1]) {
+					[this.collection[j], this.collection[j + 1]] = [
+						this.collection[j + 1],
+						this.collection[j],
+					];
+					isSwapped = true;
+				}
+			}
+
+			if (!isSwapped) break;
+		}
+	}
 }
 
 const sorter = new Sorter([10, 3, -5, 0]);
